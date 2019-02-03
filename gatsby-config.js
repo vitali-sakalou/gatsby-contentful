b38c8381e152f2284294dbf8c19e1abb1103c1b8
unknown <vitali.sakalou@gmail.com>
2019-02-02 18:38:17 -0800

    second commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,16 +2,23 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@gatsbyjs`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `6f4ashlw7bcx`,
+        accessToken: `0d7ff711786ccc88f817d4b9882530f3550e5bef7f0aa0654b89ac4099e272d8`
+      }
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -24,11 +31,11 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
+        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
-  ],
-}
+  ]
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,42 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+// import { graphql } from "gatsby";
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import { Link } from "gatsby";
 
-const IndexPage = () => (
+import Layout from "../components/layout";
+import Image from "../components/image";
+import SEO from "../components/seo";
+
+const IndexPage = ({ data }) => (
   <Layout>
+    {console.log("!!!", data)}
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
+    <h1>Hi people!</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <img src={data.contentfulAnime.img.fixed.src} />
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
-)
+);
 
-export default IndexPage
+export const query = graphql`
+  query HomePageQuery {
+    contentfulCategory {
+      slug
+    }
+    contentfulAnime {
+      vitTitle
+      img {
+        id
+        fixed {
+          src
+        }
+      }
+    }
+  }
+`;
+
+export default IndexPage;
